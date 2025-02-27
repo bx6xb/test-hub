772cdab0d97dccb2801fa853bc2ec04b1adbd62b
Yan Turnt <bx6xbchosen1@gmail.com>
2025-02-27 17:06:18 +0500

    fix: fixed getTimeFromIsoDate logic

--- src/shared/lib/getTimeFromIsoDate.ts
+++ src/shared/lib/getTimeFromIsoDate.ts
@@ -1,4 +1,7 @@
 export const getTimeFromIsoDate = (isoDate: string): string => {
   const date = new Date(isoDate)
-  return `${date.getUTCHours()}:${date.getUTCMinutes()}`
+  const hours = date.getHours().toString().padStart(2, '0')
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+
+  return `${hours}:${minutes}`
 }
